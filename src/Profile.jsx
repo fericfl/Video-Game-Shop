aaf73656b69395f2ea82d93317175f65cd4d6a07
fericfl <ericflaviu.florea@gmail.com>
2023-12-01 16:13:33 +0200

    " "

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import GetCurrentUser from './functions/GetCurrentUser'
-import { updateDoc } from 'firebase/firestore';
+import { doc, updateDoc, deleteDoc } from 'firebase/firestore';
 import { sendPasswordResetEmail } from 'firebase/auth';
 import {db, auth} from "./functions/firebase"
 
@@ -11,28 +11,35 @@ const Profile = () => {
 
   const [username, setName] = useState(user ? user[0].username : '');
   const [email, setEmail] = useState(user ? user[0].email : '');
+  const [emailSuccessMsg, setEmailSuccessMsg] = useState();
+  const [emailErrorMsg, setEmailErrorMsg] = useState();
+  const [nameSuccessMsg, setNameSuccessMsg] = useState();
+  const [nameErrorMsg, setNameErrorMsg] = useState();
 
   const handleUpdateName = async () => {
     try {
-      await updateDoc(doc(dc, 'users', user.id), { username: username });
-      alert('Name updated successfully!');
+      await updateDoc(doc(db, 'users', user[0].id), { username: username });
+      setNameSuccessMsg('Name updated successfully. Refresh the page')      
     } catch(error) {
         console.error('Error updating name:', error.message);
+        setNameErrorMsg('Something went wrong!');
     }
   };
+  
 
   const handleUpdateEmail = async () => {
     try {
-      await updateDoc(doc(db, 'users', user.id), {email: email });
-      alert('Email updated successfully!');
+      await updateDoc(doc(db, 'users', user[0].id), {email: email });
+      setEmailSuccessMsg('Email updated successfully! Refresh the page to see update')
     } catch(error) {
       console.error('Error updating email:', error.message);
+      setEmailErrorMsg('There was an error updating the email');
     } 
   };
 
   const handleResetPassword = async () => {
     try {
-      await sendPasswordResetEmail(auth, email);
+      await sendPasswordResetEmail(auth, user[0].email);
       alert('Password reset email sent. Check your email for further instructions!');
     } catch(error) {
       console.error('Eror sending password reset email:', error.message);
@@ -41,9 +48,9 @@ const Profile = () => {
 
   const handleDeleteAccount = async () => {
     try {
-      await deleteDoc(doc(db, 'users', user.id));
+      await deleteDoc(doc(db, 'users', user[0].id));
 
-      await auth.currrentUser.delete();
+      await auth.currentUser.delete();
 
       alert('Account deleted successfully!');
     } catch(error) {
@@ -54,47 +61,86 @@ const Profile = () => {
   const handleViewOrderHistory = () => {
     alert('Navigating to order history...');
   };
+
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      alert('Logged out successfully!');
+    } catch (error) {
+      console.error('Error logging out:', error.message);
+    }
+  };
   
   if(!user) {
-    return <p>Loading...</p>;
+      return (
+        <div className='profile-container'>
+          <h1>You are not signed in.</h1>
+          <p>
+          <Link to="/signup" id="links">Signup</Link> or <Link to="/login" id="links">Login</Link>
+          </p>
+        </div>
+        
+      );
   }
-  console.log(user);
 
   return (
     <div className='profile-container'>
-      <p>Your Profile</p>
-        <Link to="/signup" id="navbarButton">Signup</Link>
-        <Link to="/login" id="navbarButton">Login</Link>
-        <h1>User Profile</h1>
-      <p>Name: {user[0].username}</p>
-      <p>Email: {user[0].email}</p>
+      <form className='profile-form'>
+          
 
-      <div>
-        <label>
-          New Name:
-          <input type="text" value={username} onChange={(e) => setName(e.target.value)} />
-        </label>
-        <button onClick={handleUpdateName}>Update Name</button>
-      </div>
+          <div>
+              <h1>Your Profile</h1>
+                <nav className='loginBar'>
+                    <Link to="/signup" id="links">Signup</Link> <Link to="/login" id="links">Login</Link>
+                </nav>
+                
+                <h1>User Profile</h1>
+              <p>Name: {user[0].username}</p>
+              <p>Email: {user[0].email}</p>
+              <label>
+                  New Name: <input type="text" value={username} onChange={(e) => setName(e.target.value)} />
+              </label>
+              <button type='button' onClick={handleUpdateName}>Update Name</button>
+              {nameSuccessMsg && (
+                <div className='success-msg'>
+                  {nameSuccessMsg}
+                </div>
+              )}
+              {nameErrorMsg && (
+                <div className='error-msg'>
+                    {nameErrorMsg}
+                </div>
+              )}
+          </div>
 
-      <div>
-        <label>
-          New Email:
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <button onClick={handleUpdateEmail}>Update Email</button>
-      </div>
+              
+      
 
-      <div>
-        <div>
-          <button onClick={handleResetPassword}>Reset Password</button>
-        </div>
-      </div>
+          <div>
+            <label>
+              New Email: <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+              {emailSuccessMsg && (
+                <div className='success-msg'>
+                  {emailSuccessMsg}
+                </div>
+              )}
+              {emailErrorMsg && (
+                <div className='error-msg'>
+                    {emailErrorMsg}
+                </div>
+              )}
+            </label>
+            <button type='button' onClick={handleUpdateEmail}>Update Email</button>  <button type='button' onClick={handleResetPassword}>Reset Password</button>
+              
+          </div>
 
-      <div>
-        <button onClick={handleDeleteAccount}>Delete Account</button>
-        <button onClick={handleViewOrderHistory}>View Order History</button>
-      </div>     
+
+          <div>
+            <button type='button' onClick={handleDeleteAccount}>Delete Account</button> <button type='button' onClick={handleViewOrderHistory}>View Order History</button> <button type='button' onClick={handleLogout}>Log Out</button>
+          </div>   
+
+            
+      </form>
     </div>
   )
 }
